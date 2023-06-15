1bea185afa5451eaf519089be5747063d2619a77
Harish.A.R <arharish496@gmail.com>
2023-06-15 16:32:48 +0530

    inter change verified and added

---
+++ INTER_CHANGE/MDK-ARM/RTE/_INTER_CHANGE/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'INTER_CHANGE' 
+ * Target:  'INTER_CHANGE' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
